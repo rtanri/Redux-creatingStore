6c98030d520293e61049dcd1eb88d4a7c35c9646
Revin Tanri <tanrirevin@gmail.com>
2020-04-22 21:20:35 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+function createStore(){
+// the store should have 4 parts:
+// 1. the state
+// 2. Get the State - return the state - getState
+// 3. Listen to change on the state - subscribe
+// 4. Update the state
+
+    let state 
+    let listeners =[]
+
+    // Create a way to get access to (1) stage
+    const getState = () => state
+
+    const subscribe =(listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listener.filter((l) => l !== listener)
+        }
+    }
+
+    // Whenever the invoke createStore(), they get an object back with return.
+    return {
+        getState,
+        subscribe
+    }
+}
+
+{
+    type: "ADD_TODO"  //type - let REDUX know exactly what event took place
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+}
+
+{
+    type:"REMOVE_TODO"
+    id:0
+}
+
+{
+    type: "TOGGLE_TODO"
+    id: 0
+}
+
+{
+    type: "ADD_GOAL"
+    goal: {
+        id: 0,
+        name: "Run a Marathon",
+    }
+}
+
+{
+    type: "REMOVE_GOAL"
+    id:0
+}
