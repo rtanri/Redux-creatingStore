2f6ee584044f06b00fff615c879d3adf5d38c944
Revin Tanri <tanrirevin@gmail.com>
2020-04-25 15:40:40 +0800

    Empty all the dispatch actions in JS & HTML

--- index.js
+++ index.js
@@ -126,33 +126,33 @@ store.subscribe(() => {
 })
 
 
-store.dispatch(addTodoAction({
-    id: 0,
-    name: 'Read books',
-    complete: true,
-})) 
+// store.dispatch(addTodoAction({
+//     id: 0,
+//     name: 'Read books',
+//     complete: true,
+// })) 
 
 
-store.dispatch(addTodoAction({
-    id: 1,
-    name: 'Wash the car',
-    complete: false,
-})) 
+// store.dispatch(addTodoAction({
+//     id: 1,
+//     name: 'Wash the car',
+//     complete: false,
+// })) 
 
-store.dispatch(removeTodoAction(1))
+// store.dispatch(removeTodoAction(1))
 
-store.dispatch(toggleTodoAction(0))
+// store.dispatch(toggleTodoAction(0))
 
-store.dispatch(addGoalAction({
-    id: 0,
-    name: 'Learn Redux'
-}))
+// store.dispatch(addGoalAction({
+//     id: 0,
+//     name: 'Learn Redux'
+// }))
 
-store.dispatch(addGoalAction({
-    id: 1,
-    name: 'Lose 20 pounds'
-}))
+// store.dispatch(addGoalAction({
+//     id: 1,
+//     name: 'Lose 20 pounds'
+// }))
 
-store.dispatch(removeGoalAction(1))
+// store.dispatch(removeGoalAction(1))
 
 
