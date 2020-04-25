d012d2e5753af4aab76e7dc3f281e1e761e9b279
Revin Tanri <tanrirevin@gmail.com>
2020-04-25 13:15:13 +0800

    Add REDUCER Function as argument in createStore()

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+//1. This is library code.
+function createStore(reducer){
+    // the store should have 4 parts:
+    // 1. the state
+    // 2. Get the State - return the state - getState
+    // 3. Listen to change on the state - subscribe
+    // 4. Update the state
+    
+        let state 
+        let listeners =[]
+    
+        // Create a way to get access to (1) stage
+        const getState = () => state
+    
+        const subscribe =(listener) => {
+            listeners.push(listener)
+            return () => {
+                listeners = listener.filter((l) => l !== listener)
+            }
+        }
+    
+        //Dispatch can pass 'state' and 'action' to the PureFunction(todo).
+        const dispatch = (action) => {
+            state = reducer(state, action) //call the PURE Function
+            listeners.forEach((listener) => listener())
+        }
+    
+        // Whenever the invoke createStore(), they get an object back with return.
+        return {
+            getState,
+            subscribe,
+            dispatch, //when you put 'dispatch' here, when you call createStore() - it will auto call dispatch
+        }
+    }
+    
+
+//2. This is App Code
 //REDUCER: PURE FUNCTION that import state & action, then reducing that to a brand new state.
 //first this function invoked, 'state' is undefined, that's why we put 'state=[]'
 function todos (state=[], action) {
@@ -7,42 +44,6 @@ function todos (state=[], action) {
 
 
     return state //brand new state created by REDUCER
-}
-
-
-
-function createStore(){
-// the store should have 4 parts:
-// 1. the state
-// 2. Get the State - return the state - getState
-// 3. Listen to change on the state - subscribe
-// 4. Update the state
-
-    let state 
-    let listeners =[]
-
-    // Create a way to get access to (1) stage
-    const getState = () => state
-
-    const subscribe =(listener) => {
-        listeners.push(listener)
-        return () => {
-            listeners = listener.filter((l) => l !== listener)
-        }
-    }
-
-    //Dispatch can pass 'state' and 'action' to the PureFunction(todo).
-    const dispatch = (action) => {
-        state = todos(state, action) //call the PURE Function
-        listeners.forEach((listener) => listener())
-    }
-
-    // Whenever the invoke createStore(), they get an object back with return.
-    return {
-        getState,
-        subscribe,
-        dispatch, //when you put 'dispatch' here, when you call createStore() - it will auto call dispatch
-    }
 }
 
 
